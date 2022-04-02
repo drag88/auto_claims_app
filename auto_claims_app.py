9c96dea8c964263c1515e5063c6859aff428a19f
drag88 <awinsreenivas@gmail.com>
2022-04-02 21:46:12 +0800

    Adding damage models with claims amount as config

--- auto_claims_app.py
+++ auto_claims_app.py
@@ -1,3 +1,4 @@
+from re import A
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -7,29 +8,37 @@ import pathlib
 from fastai.vision.widgets import *
 from fastai.vision.all import *
 from utils.ui import *
-#from models.helpers import *
-#from models import helpers
 from helpers import *
-from models.prediction import Predict
-# from fastbook import *
+from string import Template
+from collections import defaultdict
+pathlib.PosixPath = pathlib.WindowsPath
+
+# @st.cache(allow_output_mutation = True)
+def get_learners():
+    pred_dict = {i.stem : load_learner(i).predict(img)[0] for i in (Path()/'models'/'pkls').ls()}
+    return pred_dict
 
 
+# Page introduction
 introduction()
+
+# Upload image
 uploaded_file = upload_image()
 
 if uploaded_file is not None:
     img = PILImage.create((uploaded_file))
     st.image(img.to_thumb(500,500), caption='Uploaded Image')
-else: 'Please upload an image'
-
-
-temp = Path()/'models'/'pkl'/'damage.pkl'
-pathlib.PosixPath = pathlib.WindowsPath
-
-car_damage = load_learner(Path()/'models'/'pkl'/'damage.pkl')
-
-if st.button('Classify'):
-    pred, pred_idx, probs = car_damage.predict(img)
-    st.write(f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}')
+    pred_dict = get_learners()
 else: 
-    st.write(f'Click the button to classify') 
+    st.markdown('Please upload an image')
+
+
+if st.button('Generate Claim Amount'):
+    if uploaded_file is not None:
+        get_damage_prediction(pred_dict)
+    else :
+        st.error('Please upload an image')
+else: 
+    st.write(f'Click the button to generate claim amout based on vehicle damage') 
+
+st.markdown("""<hr style="height:10px;border:none;color:#333;background-color:#333;" /> """, unsafe_allow_html=True)
